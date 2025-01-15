67d1739db577bbca20c53f3dbe7963f54e866148
Istiaq Ahsan <ahsanistiaq23@gmail.com>
2025-01-15 21:55:27 +0600

    add static stat

--- src/pages/DashboardRightBar/Admin/AdminDashboard.jsx
+++ src/pages/DashboardRightBar/Admin/AdminDashboard.jsx
@@ -1,7 +1,92 @@
+import { FaDatabase, FaDollarSign, FaFemale, FaMale } from "react-icons/fa";
+import { MdWorkspacePremium } from "react-icons/md";
+
 const AdminDashboard = () => {
   return (
     <div>
-      <h1>AdminDashboard</h1>
+      <div className="mt-12">
+        <div className="mb-12 grid gap-y-10 gap-x-6 md:grid-cols-3  flex-grow">
+          <div className="relative flex flex-col bg-clip-border rounded-xl bg-white text-gray-700 shadow-md">
+            <div
+              className={`bg-clip-border mx-4 rounded-xl overflow-hidden bg-gradient-to-tr shadow-lg absolute -mt-4 grid h-16 w-16 place-items-center from-orange-600 to-orange-400 text-white shadow-orange-500/40`}
+            >
+              <FaDatabase className="w-6 h-6 text-white" />
+            </div>
+            <div className="p-4 text-right">
+              <p className="block antialiased font-sans text-sm leading-normal font-normal text-blue-gray-600">
+                Total Biodata Count
+              </p>
+              <h4 className="block antialiased tracking-normal font-sans text-2xl font-semibold leading-snug text-blue-gray-900">
+                $120
+              </h4>
+            </div>
+          </div>
+
+          <div className="relative flex flex-col bg-clip-border rounded-xl bg-white text-gray-700 shadow-md">
+            <div
+              className={`bg-clip-border mx-4 rounded-xl overflow-hidden bg-gradient-to-tr shadow-lg absolute -mt-4 grid h-16 w-16 place-items-center from-green-600 to-green-400 text-white shadow-green-500/40`}
+            >
+              <MdWorkspacePremium className="w-6 h-6 text-white" />
+            </div>
+            <div className="p-4 text-right">
+              <p className="block antialiased font-sans text-sm leading-normal font-normal text-blue-gray-600">
+                Premium Biodata Count
+              </p>
+              <h4 className="block antialiased tracking-normal font-sans text-2xl font-semibold leading-snug text-blue-gray-900">
+                10
+              </h4>
+            </div>
+          </div>
+
+          <div className="relative flex flex-col bg-clip-border rounded-xl bg-white text-gray-700 shadow-md">
+            <div
+              className={`bg-clip-border mx-4 rounded-xl overflow-hidden bg-gradient-to-tr shadow-lg absolute -mt-4 grid h-16 w-16 place-items-center from-blue-600 to-blue-400 text-white shadow-blue-500/40`}
+            >
+              <FaMale className="w-6 h-6 text-white" />
+            </div>
+            <div className="p-4 text-right">
+              <p className="block antialiased font-sans text-sm leading-normal font-normal text-blue-gray-600">
+                Male Biodata Count
+              </p>
+              <h4 className="block antialiased tracking-normal font-sans text-2xl font-semibold leading-snug text-blue-gray-900">
+                120
+              </h4>
+            </div>
+          </div>
+
+          <div className="relative flex flex-col bg-clip-border rounded-xl bg-white text-gray-700 shadow-md">
+            <div
+              className={`bg-clip-border mx-4 rounded-xl overflow-hidden bg-gradient-to-tr shadow-lg absolute -mt-4 grid h-16 w-16 place-items-center from-pink-600 to-pink-400 text-white shadow-pink-500/40`}
+            >
+              <FaFemale className="w-6 h-6 text-white" />
+            </div>
+            <div className="p-4 text-right">
+              <p className="block antialiased font-sans text-sm leading-normal font-normal text-blue-gray-600">
+                Female Biodata Count
+              </p>
+              <h4 className="block antialiased tracking-normal font-sans text-2xl font-semibold leading-snug text-blue-gray-900">
+                120
+              </h4>
+            </div>
+          </div>
+
+          <div className="relative flex flex-col bg-clip-border rounded-xl bg-white text-gray-700 shadow-md">
+            <div
+              className={`bg-clip-border mx-4 rounded-xl overflow-hidden bg-gradient-to-tr shadow-lg absolute -mt-4 grid h-16 w-16 place-items-center from-green-600 to-green-400 text-white shadow-green-500/40`}
+            >
+              <FaDollarSign className="w-6 h-6 text-white" />
+            </div>
+            <div className="p-4 text-right">
+              <p className="block antialiased font-sans text-sm leading-normal font-normal text-blue-gray-600">
+                Total Revenue
+              </p>
+              <h4 className="block antialiased tracking-normal font-sans text-2xl font-semibold leading-snug text-blue-gray-900">
+                10
+              </h4>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
